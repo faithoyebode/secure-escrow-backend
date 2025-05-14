43e8142e8089e9b540256cf2b107685d22d5ae21
Faith Oyebode <oyebodefaith3@gmail.com>
2025-05-14 04:05:14 +0100

    fix: code fixes

--- src/data-source.ts
+++ src/data-source.ts
@@ -15,7 +15,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: process.env.DATABASE_PATH || "./escrow-platform.sqlite",
-  synchronize: process.env.NODE_ENV === "development", // Only in development
+  synchronize: true, // Only in development
   logging: process.env.NODE_ENV === "development",
   entities: [User, Product, Escrow, EscrowProduct, Dispute, DisputeComment],
   migrations: [__dirname + "/migration/**/*.ts"],
